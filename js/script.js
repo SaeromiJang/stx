9ed9624a1f0438ee0e08179302f90e0c3ed6d405
Saeromi Jang <glamorous.sky48@gmail.com>
2022-11-10 19:33:08 +0900

    안내창 생성
    안내창을 생성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    // 모달 기능
+    let modal =$('.modal');
+    let modalCon = $('.modal-content');
+    let modalClose = $('.modal-close');
+    modal.click(function(){
+        modal.stop().fadeOut();
+        $('html').css('overflow', 'auto');
+    });
+    modalCon.click(function(event){
+        // 아래로 클릭한 정보 전달 막기
+        event.stopPropagation();
+    });    
+    modalClose.click(function(){
+        modal.stop().fadeOut();
+        $('html').css('overflow', 'auto');
+    });
+
     // 게시판에 글 목록 출력
     // 공지사항 관련 데이터
     var noticeData = [{
